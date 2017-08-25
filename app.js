88785fe44129c8e71cfcb9292d96705624f2f03c
Carlos <carlosprovisorio@icloud.com>
2017-08-25 14:33:43 -0400

    Event delegation

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 //BUDGET CONTROLLER
 
 var budgetController = (function() {
@@ -116,7 +115,8 @@ var UIController = (function() {
     budgetLabel: '.budget__value',
     incomeLabel: '.budget__income--value',
     expensesLabel: '.budget__expenses--value',
-    percentageLabel: '.budget__expenses--percentage'
+    percentageLabel: '.budget__expenses--percentage',
+    container: '.container'
   };
 
   return {
@@ -199,6 +199,8 @@ var controller = (function(budgetCtrl, UICtrl) {
         crtlAddItem();
       }
     });
+
+    document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
   };
 
   var updateBudget = function() {
@@ -228,6 +230,10 @@ var controller = (function(budgetCtrl, UICtrl) {
       updateBudget();
     }
 
+  };
+
+  var ctrlDeleteItem = function(event) {
+    console.log(event.target);
   };
 
   return {
